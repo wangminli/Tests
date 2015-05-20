bb56a603a5fcb041c91ba9e5a3f1da2c8b0418da
wangminli <wongminli@163.com>
2015-05-20 15:09:08 +0800

    import module

---
+++ python/hello.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*-coding:utf-8 -*-
+
+''
+class Hello(object):
+	def sayHello(self, name='world'):
+		print('hello, %s.' % name)
---
+++ python/import_hello.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*-coding:utf-8 -*-
+
+''
+from hello import Hello
+h = Hello()
+print h.sayHello()
