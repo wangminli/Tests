1e6a08c67b7814d6ba3dc04e9f3509efcf8aef7f
wangminli <wongminli@163.com>
2015-05-28 17:28:37 +0800

    add python

--- python/fork.py
+++ python/fork.py
@@ -3,7 +3,7 @@
 
 'fork'
 import os
-print "Process(%s) start...." %os.getpid
+print "Process(%s) start...." %os.getpid()
 pid = os.fork()
 if pid == 0:
 	print 'i am child process (%s) and my parent process is (%s)' %( os.getpid(), os.getppid())
